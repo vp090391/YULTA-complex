5197cbb72680f990c053bf02dac50baf1fe8086f
vp090391 <vp090391@gmail.com>
2020-07-01 23:57:11 +0300

    Make link work correctly on Netlify

--- yulta-complex_site/src/index.js
+++ yulta-complex_site/src/index.js
@@ -10,7 +10,7 @@ import store from "./redux/redux-store";
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-         <BrowserRouter>
+         <BrowserRouter basename={process.env.PUBLIC_URL}>
               <App />
          </BrowserRouter>
       </Provider>
